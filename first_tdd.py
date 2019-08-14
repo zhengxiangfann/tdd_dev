a1a0ecbf052238fd3b1407e1e0cd49eef8807a56
xfzheng <xfzheng@hillinsight.com>
2019-08-14 17:28:20 +0800

    unittest.TestCase

--- first_tdd.py
+++ first_tdd.py
@@ -1,6 +1,23 @@
 #coding:utf-8
 
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
-assert 'Django' in browser.title
+import unittest
+# browser = webdriver.Firefox()
+# browser.get("http://localhost:8000")
+# assert 'Django' in browser.title
+
+class NewVistorTest(unittest.TestCase): #@1
+    def setUp(self): #@2
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self): # @3
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
